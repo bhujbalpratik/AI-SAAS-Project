ef77548c07523fec40bdf42e09cd80265b916877
bhujbalpratik <pratikbhujbal743@gmail.com>
2024-08-28 18:55:06 +0530

    Implement User CRUD Operations

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,39 @@
-// import {
-//   clerkMiddleware,
-//   createRouteMatcher,
-// } from "@clerk/nextjs/server"
-
-// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"])
-
-// export default clerkMiddleware((auth, request) => {
-//   if (!isPublicRoute(request)) {
-//     auth().protect()
-//   }
-// })
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-//     // Always run for API routes
-//     "/(api|trpc)(.*)",
-//   ],
-// }
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/clerk"])
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks/clerk",
+])
 
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
     auth().protect()
   }
 })
 
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 }
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+
+// const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"])
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isPublicRoute(req)) return
+//   auth().protect()
+// })
+
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
+//     "/(api|trpc)(.*)",
+//   ],
+// }
