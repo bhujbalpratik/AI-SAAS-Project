0aac407cf8b9f8dc92902fb27be93192dad4509f
bhujbalpratik <pratikbhujbal743@gmail.com>
2024-09-09 11:38:47 +0530

    Image Details update & delete functionality added

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -164,7 +164,7 @@ const TransformationForm = ({
           [fieldName === "prompt" ? "prompt" : "to"]: value,
         },
       }))
-    }, 1000)
+    }, 1000)()
     return onChangeField(value)
   }
 
@@ -211,6 +211,7 @@ const TransformationForm = ({
                 onValueChange={(value) =>
                   onSelectFieldHandler(value, field.onChange)
                 }
+                value={field.value}
               >
                 <SelectTrigger className="select-field">
                   <SelectValue placeholder="Select size" />
